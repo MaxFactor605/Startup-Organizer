3d3e790aa410045bb9187612e498a4a17fcc9899
MaxFactor605 <aleksandrow.max@gmail.com>
2020-05-24 21:17:23 +0300

    _

--- suorganizer/settings/production.py
+++ suorganizer/settings/production.py
@@ -8,6 +8,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 DATABASES = {
     'default': dj_database_url.config()
