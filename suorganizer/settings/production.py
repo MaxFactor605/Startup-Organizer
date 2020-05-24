e52ed22c7105b19dc31b45ce5633a07e65445bbd
MaxFactor605 <aleksandrow.max@gmail.com>
2020-05-24 20:56:39 +0300

    _

--- suorganizer/settings/production.py
+++ suorganizer/settings/production.py
@@ -8,8 +8,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+'8$2(j1asy+2t113v%7%#qdntydt_@voxs3rz#7)&2u+66i!y0f'
